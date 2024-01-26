ecce3b51a20f26ad39842e14db477ed84da73199
Acer Cristea <acristea@ucsc.edu>
2024-01-26 15:48:51 -0800

    first commit

---
+++ src/Movement.js
@@ -0,0 +1,89 @@
+class Movement extends Phaser.Scene {
+    constructor() {
+        super('movementScene')
+    }
+
+    init() {
+        this.PLAYER_VELOCITY = 350
+    }
+
+    preload() {
+        this.load.spritesheet("character", "./assets/spritesheets/Character_002.png",{
+            frameWidth: 48
+        })
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0xDDDDDD)
+
+        //animate
+        this.anims.create({
+            key: "idle-down",
+            frameRate: 0,
+            repeat: -1, //infinite repeat
+            frames: this.anims.generateFrameNumbers("character", {
+                start: 1,
+                end: 1
+            })
+        })
+
+        this.anims.create({ //walking
+            key: "walk-down",
+            frameRate: 5,
+            repeat: -1, //infinite repeat
+            frames: this.anims.generateFrameNumbers("character", {
+                start: 0,
+                end: 2
+            })
+        })
+
+        this.player = this.physics.add.sprite(width/2,height/2,"character",1).setScale(2)
+
+        this.player.body.setCollideWorldBounds(true)
+
+        this.player.body.setSize(32,32).setOffset(8, 16)
+
+        cursors = this.input.keyboard.createCursorKeys()
+
+    }
+
+    update() {
+
+        let playerVector = new Phaser.Math.Vector2(0,0)
+
+        let playerDirection = "down"
+
+        //handle left and right
+        if (cursors.left.isDown) {
+            playerVector.x = -1
+            playerDirection = "left"
+        }
+
+        if (cursors.right.isDown) {
+            playerVector.x = 1
+            playerDirection = "right"
+        }      
+
+        //handle up and down
+        if (cursors.up.isDown) {
+            playerVector.y = -1
+            playerDirection = "up"
+        }
+
+        if (cursors.down.isDown) {
+            playerVector.y = 1
+            playerDirection = "down"
+        }  
+
+        playerVector.normalize()
+
+        // this.player.x += playerVector.x * this.PLAYER_VELOCITY
+        // this.player.y += playerVector.y * this.PLAYER_VELOCITY
+
+        this.player.setVelocity(this.PLAYER_VELOCITY * playerVector.x, this.PLAYER_VELOCITY * playerVector.y)
+
+        let playerMovement
+        playerVector.length() ? playerMovement = "walk" : playerMovement = "idle" //terminary operator
+        this.player.play(playerMovement + "-" + playerDirection, true)
+    }
+}
